86247e6044597d0c5f22abcfad66b23ec96dc65c
Alface <pedrocesar1410@gmail.com>
2018-12-01 15:37:39 -0200

    interface e adicao das funcoes bubbleSort e quickSort para sort js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,60 @@
-console.log(selectionSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
-console.log(insertionSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+const randomArray = (length = 50) => {
+    let array = [], availableNumbers = [];
+    for(let i = 0; i < length; i++)
+        availableNumbers.push(i + 1);
+    for(let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+        array.push(availableNumbers[randomIndex]);
+        availableNumbers.splice(randomIndex, 1);
+    }
+    return array;
+}
+
+let array = randomArray();
+
+const arrayEl = document.querySelector('#array');
+
+const renderElement = value => {
+    const element = document.createElement('div');
+    element.classList.add('element');
+    element.innerHTML = value;
+    arrayEl.appendChild(element);
+};
+
+const renderArray = () => {
+    arrayEl.innerHTML = '';
+    array.forEach(renderElement);
+};
+
+renderArray();
+
+const buttonRandomArrayEl = document.querySelector('#random-array');
+const buttonSelectionSortEl = document.querySelector('#selection-sort');
+const buttonInsertionSortEl = document.querySelector('#insertion-sort');
+const buttonBubbleSortEl = document.querySelector('#bubble-sort');
+const buttonQuickSortEl = document.querySelector('#quick-sort');
+
+buttonRandomArrayEl.onclick = () => {
+    array = randomArray();
+    renderArray();
+}
+
+buttonSelectionSortEl.onclick = () => {
+    selectionSort(array);
+    renderArray();
+}
+
+buttonInsertionSortEl.onclick = () => {
+    insertionSort(array);
+    renderArray();
+}
+
+buttonBubbleSortEl.onclick = () => {
+    bubbleSort(array);
+    renderArray();
+}
+
+buttonQuickSortEl.onclick = () => {
+    quickSort(array);
+    renderArray();
+}
--- js/sort.js
+++ js/sort.js
@@ -21,3 +21,33 @@ const insertionSort = array => {
         }
     return array;
 }
+
+const bubbleSort = array => {
+    let ordered;
+    do {
+        ordered = true;
+        for(let i = 1; i < array.length; i++) {
+            if(array[i] < array[i - 1]) {
+                const aux = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = aux;
+                ordered = false;
+            }
+        }
+    } while(!ordered);
+    return array;
+}
+
+const quickSort = (array, left = 0, right = array.length - 1) => {
+    let pivot = left;
+    for(let i = left + 1; i <= right; i++) {
+        if(array[i] < array[pivot]) {
+            const aux = array[i];
+            for(j = i; j > pivot; j--)
+                array[j] = array[j - 1];
+            array[pivot++] = aux;
+        }
+    }
+    if(pivot > left + 1) quickSort(array, left, pivot - 1);
+    if(pivot < right - 1) quickSort(array, pivot + 1, right);
+}
